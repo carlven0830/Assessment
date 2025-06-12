ab6b25dda64f631cacb6b217fcae8c3a0297a76d
carlven <tekkcarlven@gmail.com>
2025-06-12 08:58:11 +0800

    update project request

---
+++ JrAssessment.Model/Requests/UpdateProjectRequest.cs
@@ -0,0 +1,10 @@
+﻿using JrAssessment.Model.Enums;
+
+namespace JrAssessment.Model.Requests
+{
+    public class UpdateProjectRequest : AddProjectRequest
+    {
+        public Guid Id { get; set; }
+        public StatusEnum Status { get; set; }
+    }
+}
